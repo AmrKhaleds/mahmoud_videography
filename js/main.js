1c0e843368e083a0b8e57195b88e164f7da7acd0
AmrKhaledS <amrkhaleds898@gmail.com>
2021-03-17 13:45:55 +0200

    End Banner section , and add stiky navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-ECHO is on.
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // toggle menu/navbar script 
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
