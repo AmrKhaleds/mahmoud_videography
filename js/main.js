48d9366458f0209267e709f4911078ab00be6e0a
AmrKhaledS <amrkhaleds898@gmail.com>
2021-03-18 22:02:53 +0200

    add scroll bar style modified navber to came from top

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
+            $('body').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+            $('body').removeClass("sticky");
         }
     });
+
 
     // toggle menu/navbar script 
     $('.menu-btn').click(function(){
